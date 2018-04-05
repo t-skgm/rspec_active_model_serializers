00c1edd3baa74e128069b27b647f78107b11a1a2
t-skgm <skgm0301@gmail.com>
2018-04-05 14:10:45 +0900

    support active_model_serializers v0.10.x

--- lib/rspec/active_model_serializers/matchers.rb
+++ lib/rspec/active_model_serializers/matchers.rb
@@ -2,6 +2,6 @@ require 'rspec/core'
 require 'rspec/active_model_serializers/matchers/render_serializer'
 
 RSpec.configure do |config|
-  config.include ActionController::SerializationAssertions, type: :controller
+  config.include ActiveModelSerializers::Test::Serializer, type: :controller
   config.include RSpec::ActiveModelSerializers::Matchers::RenderSerializer
 end
